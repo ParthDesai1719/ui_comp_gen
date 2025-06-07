52aaea8c9544c8a1809437813189f893d2c0292b
Parth Desai <parthdesai1719@gmail.com>
2025-06-07 06:40:29 +0530

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,4 +1,8 @@
 from datasets import load_dataset
+from data.dataset_formatter import format_fluent_dev_chat
 
-def get_dataset(name="justmalhar/fluent-dev"):
-    return load_dataset(name)
+def get_dataset():
+    dataset = load_dataset("justmalhar/fluent-dev")
+    formatted = format_fluent_dev_chat(dataset)
+    print("Dataset loaded and formatted")
+    return formatted
